8bc6e4234477932d57d5d158614c9e6fc1fdf747
Alberto Pastor <brokerp18@gmail.com>
2025-12-04 20:07:44 +0100

    Start TCP server

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment the code below to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    print("Server started on port 6379")
+    client_socket, addr = server_socket.accept()
+    print(f"Connected by {addr}")
+    
+    client_socket.close()
+    server_socket.close()
 
 
 if __name__ == "__main__":
